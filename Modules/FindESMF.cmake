47ba6358e85a39978424aeb18e89e044c8a3a7cf
Benjamin Cash <bcash@c208-004.frontera.tacc.utexas.edu>
2024-09-12 10:50:34 -0500

    Build with container spack-stack-1.8.0

--- Modules/FindESMF.cmake
+++ Modules/FindESMF.cmake
@@ -1,34 +1,34 @@
 # - Try to find ESMF
 #
-# Requires setting ESMFMKFILE to the filepath of esmf.mk. If this is NOT set,
-# then ESMF_FOUND will always be FALSE. If ESMFMKFILE exists, then ESMF_FOUND=TRUE
-# and all ESMF makefile variables will be set in the global scope. Optionally,
-# set ESMF_MKGLOBALS to a string list to filter makefile variables. For example,
-# to globally scope only ESMF_LIBSDIR and ESMF_APPSDIR variables, use this CMake
-# command in CMakeLists.txt:
+# Uses ESMFMKFILE to find the filepath of esmf.mk. If this is NOT set, then this
+# module will attempt to find esmf.mk. If ESMFMKFILE exists, then
+# ESMF_FOUND=TRUE and all ESMF makefile variables will be set in the global
+# scope. Optionally, set ESMF_MKGLOBALS to a string list to filter makefile
+# variables. For example, to globally scope only ESMF_LIBSDIR and ESMF_APPSDIR
+# variables, use this CMake command in CMakeLists.txt:
 #
 #   set(ESMF_MKGLOBALS "LIBSDIR" "APPSDIR")
 
-
-# Add the ESMFMKFILE path to the cache if defined as system env variable
-if(DEFINED ENV{ESMFMKFILE} AND NOT DEFINED ESMFMKFILE)
-  set(ESMFMKFILE $ENV{ESMFMKFILE} CACHE FILEPATH "Path to ESMF mk file")
-endif()
-
-# If it's not explicitly set try to find esmf.mk file in default locations (ESMF_ROOT, CMAKE_PREFIX_PATH, etc)
+# Set ESMFMKFILE as defined by system env variable. If it's not explicitly set
+# try to find esmf.mk file in default locations (ESMF_ROOT, CMAKE_PREFIX_PATH,
+# etc)
 if(NOT DEFINED ESMFMKFILE)
-  find_path(ESMFMKFILE_PATH esmf.mk PATH_SUFFIXES lib lib64)
-  if(ESMFMKFILE_PATH)
-    set(ESMFMKFILE ${ESMFMKFILE_PATH}/esmf.mk)
-    message(STATUS "Found esmf.mk file ${ESMFMKFILE}")
+  if(NOT DEFINED ENV{ESMFMKFILE})
+    find_path(ESMFMKFILE_PATH esmf.mk PATH_SUFFIXES lib lib64)
+    if(ESMFMKFILE_PATH)
+      set(ESMFMKFILE ${ESMFMKFILE_PATH}/esmf.mk)
+      message(STATUS "Found esmf.mk file ${ESMFMKFILE}")
+    endif()
   else()
-    message(STATUS "ESMFMKFILE not defined. This is the path to esmf.mk file. \
-Without this filepath, ESMF_FOUND will always be FALSE.")
+    set(ESMFMKFILE $ENV{ESMFMKFILE})
   endif()
 endif()
 
 # Only parse the mk file if it is found
 if(EXISTS ${ESMFMKFILE})
+  set(ESMFMKFILE ${ESMFMKFILE} CACHE FILEPATH "Path to esmf.mk file")
+  set(ESMF_FOUND TRUE CACHE BOOL "esmf.mk file found" FORCE)
+
   # Read the mk file
   file(STRINGS "${ESMFMKFILE}" esmfmkfile_contents)
   # Parse each line in the mk file
@@ -78,58 +78,70 @@ if(EXISTS ${ESMFMKFILE})
   set(ESMF_BETA_RELEASE FALSE)
   if(ESMF_VERSION_BETASNAPSHOT MATCHES "^('T')$")
     set(ESMF_BETA_RELEASE TRUE)
-    string(REGEX REPLACE ".*beta_snapshot_*\([0-9]*\).*" "\\1" ESMF_BETA_SNAPSHOT "${ESMF_VERSION_STRING_GIT}")
-    message(STATUS "Detected ESMF Beta snapshot ${ESMF_BETA_SNAPSHOT}")
+    if(ESMF_VERSION_STRING_GIT MATCHES "^ESMF.*beta_snapshot")
+      set(ESMF_BETA_SNAPSHOT ${ESMF_VERSION_STRING_GIT})
+    elseif(ESMF_VERSION_STRING_GIT MATCHES "^v.\..\..b")
+      set(ESMF_BETA_SNAPSHOT ${ESMF_VERSION_STRING_GIT})
+    else()
+      set(ESMF_BETA_SNAPSHOT 0)
+    endif()
+    message(STATUS "Detected ESMF Beta snapshot: ${ESMF_BETA_SNAPSHOT}")
   endif()
   set(ESMF_VERSION "${ESMF_VERSION_MAJOR}.${ESMF_VERSION_MINOR}.${ESMF_VERSION_PATCH}")
 
-  separate_arguments(ESMF_F90COMPILEPATHS NATIVE_COMMAND ${ESMF_F90COMPILEPATHS})
-  foreach(ITEM ${ESMF_F90COMPILEPATHS})
-     string(REGEX REPLACE "^-I" "" ITEM "${ITEM}")
-     list(APPEND tmp ${ITEM})
-  endforeach()
-  set(ESMF_F90COMPILEPATHS ${tmp})
-
-  # Look for static library, if not found try dynamic library
-  find_library(esmf_lib NAMES libesmf.a PATHS ${ESMF_LIBSDIR})
-  if(esmf_lib MATCHES "esmf_lib-NOTFOUND")
-    unset(esmf_lib)
-    message(STATUS "Static ESMF library not found, searching for dynamic library instead")
-    find_library(esmf_lib NAMES esmf_fullylinked libesmf.so PATHS ${ESMF_LIBSDIR})
-    if(esmf_lib MATCHES "esmf_lib-NOTFOUND")
-      unset(esmf_lib)
-      message(STATUS "Neither the dynamic nor the static ESMF library was found")
-    else()
-      set(_library_type SHARED)
+  # Find the ESMF library
+  if(USE_ESMF_STATIC_LIBS)
+    find_library(ESMF_LIBRARY_LOCATION NAMES libesmf.a PATHS ${ESMF_LIBSDIR} NO_DEFAULT_PATH)
+    if(ESMF_LIBRARY_LOCATION MATCHES "ESMF_LIBRARY_LOCATION-NOTFOUND")
+      message(WARNING "Static ESMF library (libesmf.a) not found in \
+                       ${ESMF_LIBSDIR}. Try setting USE_ESMF_STATIC_LIBS=OFF")
+    endif()
+    if(NOT TARGET ESMF::ESMF)
+      add_library(ESMF::ESMF STATIC IMPORTED)
     endif()
   else()
-    set(_library_type STATIC)
+    find_library(ESMF_LIBRARY_LOCATION NAMES esmf PATHS ${ESMF_LIBSDIR} NO_DEFAULT_PATH)
+    if(ESMF_LIBRARY_LOCATION MATCHES "ESMF_LIBRARY_LOCATION-NOTFOUND")
+      message(WARNING "ESMF library not found in ${ESMF_LIBSDIR}.")
+    endif()
+    if(NOT TARGET ESMF::ESMF)
+      add_library(ESMF::ESMF UNKNOWN IMPORTED)
+    endif()
   endif()
 
-  string(STRIP "${ESMF_F90ESMFLINKRPATHS} ${ESMF_F90ESMFLINKPATHS} ${ESMF_F90LINKPATHS} ${ESMF_F90LINKLIBS} ${ESMF_F90LINKOPTS}" ESMF_INTERFACE_LINK_LIBRARIES)
-  set(ESMF_LIBRARY_LOCATION ${esmf_lib})
+  # Add ESMF as an alias to ESMF::ESMF for backward compatibility
+  if(NOT TARGET ESMF)
+    add_library(ESMF ALIAS ESMF::ESMF)
+  endif()
+
+  # Add ESMF include directories
+  set(ESMF_INCLUDE_DIRECTORIES "")
+  separate_arguments(_ESMF_F90COMPILEPATHS UNIX_COMMAND ${ESMF_F90COMPILEPATHS})
+  foreach(_ITEM ${_ESMF_F90COMPILEPATHS})
+    string(REGEX REPLACE "^-I" "" _ITEM "${_ITEM}")
+    list(APPEND ESMF_INCLUDE_DIRECTORIES ${_ITEM})
+  endforeach()
+
+  # Add ESMF link libraries
+  string(STRIP "${ESMF_F90LINKRPATHS} ${ESMF_F90ESMFLINKRPATHS} ${ESMF_F90ESMFLINKPATHS} ${ESMF_F90LINKPATHS} ${ESMF_F90LINKLIBS} ${ESMF_F90LINKOPTS}" ESMF_INTERFACE_LINK_LIBRARIES)
+
+  # Finalize find_package
+  include(FindPackageHandleStandardArgs)
+
+  find_package_handle_standard_args(
+        ${CMAKE_FIND_PACKAGE_NAME}
+        REQUIRED_VARS ESMF_LIBRARY_LOCATION
+                      ESMF_INTERFACE_LINK_LIBRARIES
+                      ESMF_F90COMPILEPATHS
+        VERSION_VAR ESMF_VERSION)
+
+  set_target_properties(ESMF::ESMF PROPERTIES
+        IMPORTED_LOCATION "${ESMF_LIBRARY_LOCATION}"
+        INTERFACE_INCLUDE_DIRECTORIES "${ESMF_INCLUDE_DIRECTORIES}"
+        INTERFACE_LINK_LIBRARIES "${ESMF_INTERFACE_LINK_LIBRARIES}")
 
 else()
-
-  message(WARNING "ESMFMKFILE ${ESMFMKFILE} does not exist")
-
-endif()
-
-## Finalize find_package
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(
-    ${CMAKE_FIND_PACKAGE_NAME}
-    REQUIRED_VARS ESMF_LIBRARY_LOCATION
-                  ESMF_INTERFACE_LINK_LIBRARIES
-                  ESMF_F90COMPILEPATHS
-    VERSION_VAR ESMF_VERSION)
-
-## If ESMF is found create imported library target
-if(ESMF_FOUND)
-  add_library(esmf ${_library_type} IMPORTED)
-  set_target_properties(esmf PROPERTIES
-    IMPORTED_LOCATION "${ESMF_LIBRARY_LOCATION}"
-    INTERFACE_INCLUDE_DIRECTORIES "${ESMF_F90COMPILEPATHS}"
-    INTERFACE_LINK_LIBRARIES "${ESMF_INTERFACE_LINK_LIBRARIES}")
+  set(ESMF_FOUND FALSE CACHE BOOL "esmf.mk file NOT found" FORCE)
+  message(WARNING "ESMFMKFILE ${ESMFMKFILE} not found. Try setting ESMFMKFILE \
+                   to esmf.mk location.")
 endif()
